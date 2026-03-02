686f9dd35b4e7f31d3046140abfb12f9924739c5
sthabibek <bibek.shrestha.mirai.two006@gmail.com>
2026-03-02 20:28:02 +0545

    Practiced about Declaration and Initialization

--- script.js
+++ script.js
@@ -5,9 +5,25 @@
 const PI = 3.14;
 let age = 19;
 
-console.log("Age:", age);
+console.log("Pie Value is : ", PI);
+console.log("Age : " , age);
 
-age = 20; // reassign allowed
-console.log("Updated Age:", age);
+age = age += 1; // Reassign allowed in let variable
+console.log("Age : " , age);
 
+/*
+PI = 3.16; // Ressign  is not allowed in const variable because it is constable variable
+console.log(PI);
+*/
 
+// ===============================
+// 2. DECLARATION & INITIALIZATION
+// ===============================
+
+let username; // Declaration
+username = "Bibek Shrestha"; // Initialization
+
+let city = "Kathmandu" // Declaration and Initialization at a same time 
+
+console.log("Username : " , username);
+console.log("City : " , city);
