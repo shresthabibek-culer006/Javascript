240177f08f51b54644489d8bfe1e4441e23c6998
Bibek <bibek.shrestha.mirai.two006@gmail.com>
2026-03-20 23:27:39 +0545

    remove duplicates from array

--- script.js
+++ script.js
@@ -972,6 +972,14 @@ function findOldest(people) {
 
 console.log(findOldest(people));  // {name: "Shyam", age: 30}
 
+// Remove duplicate values from array
+function removeDuplicates(arr) {
+    return [...new Set(arr)];
+}
+
+console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5]));  // [1, 2, 3, 4, 5]
+console.log(removeDuplicates(["a", "b", "a", "c"]));   // ["a", "b", "c"]
+
 // ===============================
 // END OF PRACTICE
 // ===============================
