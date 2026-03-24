1f59dd4016a5eaebb06e2a81773b71e537776079
Bibek <bibek.shrestha.mirai.two006@gmail.com>
2026-03-24 09:00:00 +0545

    chunk array into smaller arrays

--- learn.js
+++ learn.js
@@ -36,3 +36,14 @@ function isVowel(char) {
 
 console.log(isVowel("a")); // true
 console.log(isVowel("b")); // false
+
+
+function chunkArray(arr, size) {
+    const result = [];
+    for (let i = 0; i < arr.length; i += size) {
+        result.push(arr.slice(i, i + size));
+    }
+    return result;
+}
+
+console.log(chunkArray([1, 2, 3, 4, 5, 6], 2)); // [[1,2], [3,4], [5,6]]
