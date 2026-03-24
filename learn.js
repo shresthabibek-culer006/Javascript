82f7b2d55d9944807dfb04918bc40f1ebac2ae24
Bibek <bibek.shrestha.mirai.two006@gmail.com>
2026-03-24 17:00:00 +0545

    find index of value in array

--- learn.js
+++ learn.js
@@ -80,3 +80,14 @@ function getInitials(name) {
 }
 
 console.log(getInitials("Bibek Shrestha")); // "BS"
+
+function findIndex(arr, value) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === value) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+console.log(findIndex([10, 20, 30], 20)); // 1
