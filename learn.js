d6e321f1a1cee3fe0287d05425c7060552ce9f05
Bibek <bibek.shrestha.mirai.two006@gmail.com>
2026-03-24 11:00:00 +0545

    count even and odd numbers

--- learn.js
+++ learn.js
@@ -47,3 +47,14 @@ function chunkArray(arr, size) {
 }
 
 console.log(chunkArray([1, 2, 3, 4, 5, 6], 2)); // [[1,2], [3,4], [5,6]]
+
+function countEvenOdd(arr) {
+    let even = 0, odd = 0;
+    arr.forEach(num => {
+        if (num % 2 === 0) even++;
+        else odd++;
+    });
+    return {even, odd};
+}
+
+console.log(countEvenOdd([1, 2, 3, 4, 5, 6])); // {even: 3, odd: 3}
