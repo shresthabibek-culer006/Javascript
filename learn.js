4857d56c5a67d78cdc2dff089430256d55db767a
Bibek <bibek.shrestha.mirai.two006@gmail.com>
2026-03-26 10:00:00 +0545

    convert to title case

--- learn.js
+++ learn.js
@@ -139,3 +139,11 @@ function getRandomElement(arr) {
 }
 
 console.log(getRandomElement([1, 2, 3, 4, 5]));
+
+function toTitleCase(str) {
+    return str.split(' ').map(word => 
+        word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+    ).join(' ');
+}
+
+console.log(toTitleCase("hello world")); // "Hello World"
