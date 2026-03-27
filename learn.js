03d6ab886110c75516c9d5c1c6cff18bfbfe88ae
Bibek <bibek.shrestha.mirai.two006@gmail.com>
2026-03-27 12:00:00 +0545

    find array intersection

--- learn.js
+++ learn.js
@@ -210,3 +210,8 @@ function removeFalsy(arr) {
 
 console.log(removeFalsy([0, 1, false, 2, "", 3, null, undefined, NaN])); // [1, 2, 3]
 
+function intersection(arr1, arr2) {
+    return arr1.filter(item => arr2.includes(item));
+}
+
+console.log(intersection([1, 2, 3, 4], [3, 4, 5, 6])); // [3, 4]
