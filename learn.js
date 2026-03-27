ed0c7a2967874667a7d851ac37b2e843f56b32a6
Bibek <bibek.shrestha.mirai.two006@gmail.com>
2026-03-27 10:30:00 +0545

    remove falsy values from array

--- learn.js
+++ learn.js
@@ -203,3 +203,9 @@ function fibonacci(n) {
 }
 
 console.log(fibonacci(8)); // [0, 1, 1, 2, 3, 5, 8, 13]
+
+function removeFalsy(arr) {
+    return arr.filter(Boolean);
+}
+
+console.log(removeFalsy([0, 1, false, 2, "", 3, null, undefined, NaN])); // [1, 2, 3]
