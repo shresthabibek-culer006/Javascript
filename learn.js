df07be6668b66568c202fdb4a920dd9108250857
Bibek <bibek.shrestha.mirai.two006@gmail.com>
2026-03-28 17:00:00 +0545

    check if strings are anagrams

--- learn.js
+++ learn.js
@@ -310,3 +310,11 @@ function truncate(str, maxLength) {
 }
 
 console.log(truncate("Hello World", 5)); // "Hello..."
+
+function isAnagram(str1, str2) {
+    const normalize = str => str.toLowerCase().split('').sort().join('');
+    return normalize(str1) === normalize(str2);
+}
+
+console.log(isAnagram("listen", "silent")); // true
+console.log(isAnagram("hello", "world")); // false
